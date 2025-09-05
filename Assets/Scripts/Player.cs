7ee59fb268570a1785a17f2830b1ac9fcae2286d
Fashich <thalassaaddict@gmail.com>
2025-09-05 08:17:02 +0700

    fixed bug c# and add collider physical
    - menambahkan benturan pada object yang saling berdekatan
    - memperbaiki c# configuration
    - menambahkan fisika agar tidak nembus dan tidak tenggelam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,14 +11,21 @@ public class Player : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
         //_rigidbody.MovePosition(transform.position + movementDirection * _speed * Time.deltaTime);
-        _rigidbody.velocity = movementDirection * _speed * Time.fixedDeltaTime;
+        _rigidbody.linearVelocity = movementDirection * _speed * Time.fixedDeltaTime;
         Debug.Log("Horizontal: " + horizontal);
         Debug.Log("Vertical: " + vertical);
+        if (!Physics.Raycast(transform.position, movementDirection, 0.5f))
+        {
+            _rigidbody.MovePosition(
+                transform.position +
+                movementDirection * _speed * Time.fixedDeltaTime
+            );
+        }
     }
 }
